5cdfbc9d0cbd8a302a29f0b9fb2bc4da18f223e3
Simon Ko <simonko86@gmail.com>
2017-10-25 20:50:45 -0400

    Edited

--- Assignment 1/simon_ko_a1.c
+++ Assignment 1/simon_ko_a1.c
@@ -2,6 +2,10 @@
 
 int main(int argc, char const *argv[])
 {
+	char sentence [30];
+
 	printf("hello world\n");
+	fgets(sentence,30,stdin);
+	printf("This is the sentence: %s\n", sentence);
 	return 0;
 }
