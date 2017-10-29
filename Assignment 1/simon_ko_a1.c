306dbdad8eb0282967b1955a2a9674c734a67357
Simon Ko <simonko86@gmail.com>
2017-10-28 23:01:51 -0400

    added more functionality

--- Assignment 1/simon_ko_a1.c
+++ Assignment 1/simon_ko_a1.c
@@ -3,13 +3,32 @@
 int main(int argc, char const *argv[])
 {
 	int number = 0;
+	int number2 = 0;
 	int area = 0;
 
-	printf("Please print in the length of a side?\n");
-	scanf("%d", &number);
+	printf("Is this a square or rectangle? 1 for square, 2 for rectangle\n");
 
-	area = number*number;
+	if (input == 1)
+	{
+		printf("Please print in the length of a side?\n");
+		scanf("%d", &number);
+		area = number*number;
+		printf("The are of the square is: %d\n", area );
+	}
+	else if (input == 2)
+	{
+		printf("Please print in the length of a side?\n");
+		scanf("%d", &number);
+		printf("Please print in the length of the other side?\n");
+		scanf("%d", &number2);
+		area = number*number2;
+		printf("The are of the square is: %d\n", area );
+	}
+	else
+	{
+		printf("incorrect input\n");
+	}
 
-	printf("The are of the square is: %d\n", area );
+
 	return 0;
 }
